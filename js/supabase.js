5cbb2739d8498a02196c5470769ebb481546bf73
renatobressan <re.tavares78@gmail.com>
2026-01-17 22:25:41 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,8 +1,8 @@
 // supabase.js
 // Cliente Supabase (frontend)
 
-const SUPABASE_URL = "https://SEU-PROJETO.supabase.co";
-const SUPABASE_ANON_KEY = "SUA_ANON_KEY_AQUI";
+const SUPABASE_URL = "https://fzcliwxwzkwomvfeevds.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_nJW2y3Z0E1I_cQUN67-nbg_CepDJiVQ";
 
 const supabase = window.supabase.createClient(
   SUPABASE_URL,
