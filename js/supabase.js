fddf41b8b1b6449e667183bee858d7c4d9681300
renatobressan <re.tavares78@gmail.com>
2026-02-17 15:24:44 -0300

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -14,9 +14,8 @@
 
 //console.log("Supabase conectado:", window.sb);
 
-const SUPABASE_URL = "https://fzclivwxzwkomvfeevds.supabase.co";
-
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6Y2xpd3h3emt3b212ZmVldmRzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjgxNTcwMzEsImV4cCI6MjA4MzczMzAzMX0.filx-dFt2vVF42PPcVUTNgE_n2gW9Y1vChvHsf75Xn0";
+const SUPABASE_URL = "https://fzcliwxwzkwomvfeevds.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_nJW2y3Z0E1I_cQUN67-nbg_CepDJiVQ";
 
 window.sb = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
