3d244d1a2cffe6a97f3110c638a4448d7a710d0a
renatobressan <re.tavares78@gmail.com>
2026-02-17 11:17:35 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -3,30 +3,25 @@ document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("loginForm");
   const errorMsg = document.getElementById("errorMsg");
 
-  form.addEventListener("submit", function (e) {
+  form.addEventListener("submit", async function (e) {
     e.preventDefault();
 
     const email = document.getElementById("email").value.trim();
     const password = document.getElementById("password").value.trim();
 
-    // Limpa erro anterior
-    errorMsg.style.display = "none";
+    errorMsg.textContent = "";
 
-    // Simulação de login (trocar depois por Supabase)
-    if (email === "admin@carro.com" && password === "123456") {
+    const { data, error } = await window.supabaseClient.auth.signInWithPassword({
+      email: email,
+      password: password,
+    });
 
-      // Salva sessão simples
-      localStorage.setItem("loggedIn", "true");
-
-      // Redireciona
-      window.location.href = "dashboard.html";
-
-    } else {
-
+    if (error) {
       errorMsg.textContent = "Email ou senha inválidos";
-      errorMsg.style.display = "block";
-
+      return;
     }
+
+    window.location.href = "/dashboard-carro/dashboard.html";
   });
 
 });
