dc638152261104b514105fdf55bcd06b39588701
renatobressan <re.tavares78@gmail.com>
2026-02-17 14:35:12 -0300

    Update login.js

--- js/login.js
+++ js/login.js
@@ -1,15 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+  console.log("Login JS carregado");
+  console.log("SB existe?", window.sb);
+
   const form = document.getElementById("loginForm");
 
   form.addEventListener("submit", async (event) => {
 
     event.preventDefault();
 
+    if (!window.sb) {
+      alert("Supabase não carregou!");
+      return;
+    }
+
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
-    const { error } = await sb.auth.signInWithPassword({
+    const { error } = await window.sb.auth.signInWithPassword({
       email: email,
       password: password
     });
